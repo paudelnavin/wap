0402ea8cd453418190d622ac887565fecd47e4f8
paudelnavin <navinpaudel@outlook.com>
2020-04-27 22:10:22 -0500

    minor changes

--- w2d1/decoratemytext.js
+++ w2d1/decoratemytext.js
@@ -1,10 +1,28 @@
-document.getElementById("bling").onchange = function() {
-    const body = document.body;
-    if(this.checked == true) {
-        text.className = "bling-checked";
-        body.style.background = "url('hundred-dollar-bill.jpg')";
-    } else {
-        text.className = "bling-unchecked";
-        body.style.background = "";
+window.onload = function() {
+    "use strict";
+    const myTextArea = document.getElementById("textarea");
+    const myTextAreaStyle = myTextArea.style;
+
+    function grow() {
+        let oldSize = parseInt(myTextAreaStyle.fontSize);
+        if (isNaN(oldSize)) {
+            oldSize = 12;
+        }
+        myTextAreaStyle.fontSize = (oldSize + 2) + "pt";
+    }
+
+    document.getElementById("button").onclick = function () {
+        setInterval(grow, 500);
+    };
+
+    document.getElementById("bling").onchange = function () {
+        const body = document.body;
+        if (this.checked == true) {
+            text.className = "bling-checked";
+            body.style.background = "url('hundred-dollar-bill.jpg')";
+        } else {
+            text.className = "bling-unchecked";
+            body.style.background = "";
+        }
     }
 }
