ef473270a6693572f56b6fff508b549306cb16ad
paudelnavin <navinpaudel@outlook.com>
2020-04-29 17:52:43 -0500

    some changes

--- w2d1/ASCIIAnimation/ascii.js
+++ w2d1/ASCIIAnimation/ascii.js
@@ -8,23 +8,23 @@ document.getElementById('stop').onclick = stopAnimationBtn;
 document.getElementById('turbo').onclick = speedUpTurbo;
 
 /* declare and initial public variables */
-let animationArray = [];
-let animationIntervalID;
-let speed = 250;
+var animationArray = [];
+var animationIntervalID;
+var speed = 250;
 
 /* prepare the default environment */
 toggleButtons("initial"); // disable buttons by default
 
 /* change the font size in the text area */
 function fontsize() {
-    const size = this.value;
-    const textArea = document.getElementById("text");
+    var size = this.value;
+    var textArea = document.getElementById("text");
     textArea.style.fontSize = parseInt(size) + 'pt';
 }
 /* change the animation array in the text area */
 function animation() {
-    const target = this.value;
-    const textArea = document.getElementById("text");
+    var target = this.value;
+    var textArea = document.getElementById("text");
     if(target !== "Blank"){
         animationArray = Animation[target].split("=====\n");
         toggleButtons('stop'); // active start button after the animation chose.
@@ -37,8 +37,8 @@ function animation() {
 /* start the animation in the text area */
 function startAnimationBtn() {
     toggleButtons('start');
-    const textArea = document.getElementById("text");
-    let i =0;
+    var textArea = document.getElementById("text");
+    var i =0;
     animationIntervalID = setInterval(function () {
         if (i < animationArray.length){
             textArea.textContent = animationArray[i];
@@ -71,13 +71,13 @@ function speedUpTurbo(){
 
 /* change the disable buttons when start or stop */
 function toggleButtons(clickedButton) {
-    const stop = document.getElementById("stop");
-    const start = document.getElementById("start");
+    var stop = document.getElementById("stop");
+    var start = document.getElementById("start");
 
     if (clickedButton === 'start'){
         stop.disabled = false;
         start.disabled = true;
-    }else if (clickedButton == 'stop'){
+    }else if (clickedButton === 'stop'){
         start.disabled = false;
         stop.disabled = true;
     }else{
